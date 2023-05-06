610bbfd9352c4b0cc7e94bb8ee9a531bf80d84f6
1CASH1 <cash_dip@hotmail.com>
2023-05-05 23:35:50 -0600

    Return X

--- main.py
+++ main.py
@@ -6,28 +6,30 @@ import uvicorn
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+#templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+    return "Xxx"
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+#    print('Request for index page received')
+#    return templates.TemplateResponse('index.html', {"request": request})
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+#@app.get('/favicon.ico')
+#async def favicon():
+#    file_name = 'favicon.ico'
+#    file_path = './static/' + file_name
+#    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+
+##@app.post('/hello', response_class=HTMLResponse)
+#async def hello(request: Request, name: str = Form(...)):
+#    if name:
+#        print('Request for hello page received with name=%s' % name)
+#        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+#    else:
+#        print('Request for hello page received with no name or blank name -- redirecting')
+#        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
